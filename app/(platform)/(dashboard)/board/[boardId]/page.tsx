8443d98131ed41f885544ffe54c7444f8d7253dd
Linh Pham <phamviethonglinh@gmail.com>
2024-07-12 21:22:18 +0700

    6:52:25

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const BoardIdPage = () => {
-    return (
-        <div>
-            Board ID
-        </div>
-    );
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+const BoardIdPage = async ({ params: { boardId } }: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
+  return <div>Board ID</div>;
 };
 
 export default BoardIdPage;
